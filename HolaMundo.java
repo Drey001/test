26e22d3fee59e82d96f75043b513ce593ed256df
Drey <danielrg6@gmail.com>
2020-03-08 17:20:31 -0500

    Version 1 Hola Mundo

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+
+public class HolaMundo {
+
+//    public static void main(String[] args) {
+//        System.out.println("Hola Mundo desde Java");
+//    }
+    public static void main(String[] args) {
+        System.out.println("saludos!!!");
+    }
+}
